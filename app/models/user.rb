55471d851253414d7893bc46752e2c801286bd19
Molly Risley <mollyarisley@gmail.com>
2022-11-21 15:39:15 -0600

    bcrypt install

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
   has_many :articles
   validates :username, presence: true, uniqueness: {case_sensitive: false}, length: {minimum: 3, maximum: 25}
   validates :email, presence: true, uniqueness: {case_sensitive: false}, length: {maximum: 105}, format: {with: URI::MailTo::EMAIL_REGEXP}
+  has_secure_password
 end 
