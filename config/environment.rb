2b4b0775800134346a661cad00cb2d6b11c7cb2a
Molly Risley <mollyarisley@gmail.com>
2022-11-28 14:37:14 -0600

    bootstrap alerts for error messages

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,8 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+#gets rid of field_with setting that comes with rails
+ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
+  html_tag.html_safe
+end
