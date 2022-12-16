e9601460abc228cd10dcfe431d9ae54f22ab2ce2
Molly Risley <mollyarisley@gmail.com>
2022-12-16 12:35:53 -0600

    update category actions

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -21,14 +21,18 @@ class CategoriesController < ApplicationController
 
   def show
     @category = Category.find(params[:id])
-    @articles = @category.articles
+    @articles = @category.articles.paginate(page: params[:page], per_page: 5)
   end 
+
+  def edit
+    @category = Category.find(params[:id])
+  end
 
   def update
     @category = Category.find(params[:id])
     if @category.update(category_params)
       flash[:notice] = "Category name updated successfully"
-      redirect to @category
+      redirect_to @category
     else
       render 'edit'
     end 
